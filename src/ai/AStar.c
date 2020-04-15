3084981ffbe95711fec23abec8a54331dda72ca9
QwazyWabbit <gandalf.2004@gmail.com>
2020-04-14 23:10:23 -0700

    Fix function definition to eliminate implicit int.

--- src/ai/AStar.c
+++ src/ai/AStar.c
@@ -97,7 +97,7 @@ static void AStar_InitLists (void)
 	memset( alist, -1, sizeof(alist));//jabot092
 }
 
-static int AStar_PLinkDistance( n1, n2 )
+static int AStar_PLinkDistance( int n1, int n2 )
 {
 	int	i;
 
@@ -139,7 +139,7 @@ static void AStar_PutInClosed( int node )
 	astarnodes[node].list = CLOSEDLIST;
 }
 
-static void AStar_PutAdjacentsInOpen( node )
+static void AStar_PutAdjacentsInOpen( int node )
 {
 	int	i;
 
