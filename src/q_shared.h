bfc5e42495f8076dfc93ce3b2c120b1d265552a3
QwazyWabbit <gandalf.2004@gmail.com>
2020-04-15 19:19:41 -0700

    Disable some warnings on Windows builds. Delete unused maplist.h

--- src/q_shared.h
+++ src/q_shared.h
@@ -5,15 +5,31 @@
 #define Q_SHARED
 
 #ifdef _WIN32
-// unknown pragmas are SUPPOSED to be ignored, but....
-#pragma warning(disable : 4244)     // MIPS
-#pragma warning(disable : 4136)     // X86
-#pragma warning(disable : 4051)     // ALPHA
-
-#pragma warning(disable : 4018)     // signed/unsigned mismatch
-#pragma warning(disable : 4305)		// truncation from const double to float
-
+#define NO_GDS	1
 #endif
+
+#ifdef _WIN32
+#pragma warning(disable : 4244)     // conversion from 'type1' to 'type2', possible loss of data
+#pragma warning(disable : 4100)		// unreferenced formal parameter
+#pragma warning(disable : 4127)		// conditional expression is constant
+#pragma warning(disable : 4305)		// truncation from const double to float
+#pragma warning(disable : 4018)     // signed/unsigned mismatch
+#if _MSC_VER > 1500
+#pragma warning(disable : 4996)		// unsafe CRT functions (_CRT_SECURE_NO_WARNINGS).
+#pragma warning(disable : 4459)		// declaration of 'var' hides global declaration.
+#pragma warning(disable : 6244)		// local declaration of <variable> hides previous declaration at <line> of <file>
+#endif
+#endif
+
+//#ifdef _WIN32
+//// unknown pragmas are SUPPOSED to be ignored, but....
+//#pragma warning(disable : 4244)     // MIPS
+//#pragma warning(disable : 4136)     // X86
+//#pragma warning(disable : 4051)     // ALPHA
+//
+//#pragma warning(disable : 4018)     // signed/unsigned mismatch
+//
+//#endif
 
 //K03 Begin
 #ifndef __cplusplus
