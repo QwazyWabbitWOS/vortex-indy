fc30b29a04fbae19e9f99951dece3f16cd376c6b
QwazyWabbit <gandalf.2004@gmail.com>
2020-04-29 23:13:07 -0700

    Fix another memset blanking of a buffer.

--- src/menu/trade.c
+++ src/menu/trade.c
@@ -117,11 +117,11 @@ void TradeItems(edict_t *player1, edict_t *player2)
 	{
 		char path[MAX_QPATH];
 
-		memset(path, 0, strlen(path));
+		memset(path, 0, sizeof path);
 		VRXGetPath(path, player1);
 		VSF_SaveRunes(player1, path);
 
-		memset(path, 0, strlen(path));
+		memset(path, 0, sizeof path);
 		VRXGetPath(path, player2);
 		VSF_SaveRunes(player2, path);
 
